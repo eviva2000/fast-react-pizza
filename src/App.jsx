2e4d8e3f752d1b3feab564164504eb8ed31cdbb6
eviva2000 <arjomandielaheh@gamin.com>
2024-07-08 22:06:34 +0200

    created routes with createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+
 function App() {
-  return <div>hello vite</div>;
+  // we use this imparative way in order to fetch data with react router
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/menu",
+      element: <Menu />,
+    },
+    {
+      path: "/order/new",
+      element: <CreateOrder />,
+    },
+    {
+      path: "/order/:orderId",
+      element: <Order />,
+    },
+    {
+      path: "/cart",
+      element: <Cart />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
