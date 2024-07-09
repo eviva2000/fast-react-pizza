7d798248563d1ce536881018e3870b55c090b51d
eviva2000 <arjomandielaheh@gamin.com>
2024-07-09 14:10:45 +0200

    added app layout and nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,34 @@ import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
-
+import { AppLayout } from "./ui/AppLayout";
 function App() {
-  // we use this imparative way in order to fetch data with react router
+  // we use this imparative way (createBrowserRouter) in order to fetch data/load data with react router
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/menu",
-      element: <Menu />,
-    },
-    {
-      path: "/order/new",
-      element: <CreateOrder />,
-    },
-    {
-      path: "/order/:orderId",
-      element: <Order />,
-    },
-    {
-      path: "/cart",
-      element: <Cart />,
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/menu",
+          element: <Menu />,
+        },
+        {
+          path: "/order/new",
+          element: <CreateOrder />,
+        },
+        {
+          path: "/order/:orderId",
+          element: <Order />,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+      ],
     },
   ]);
 
---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast react pizza</Link>
+    </header>
+  );
+}
+
+export default Header;
